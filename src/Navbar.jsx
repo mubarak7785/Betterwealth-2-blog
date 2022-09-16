5079d456dcf933e966ad660518b19071345ac09c
mubarak7785 <mubarakjinabade07@gmail.com>
2022-09-16 07:49:05 +0530

    created home navbar components

---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+export const Navbar=()=>{
+    return(
+
+        <nav>
+        <h1>The blog</h1>
+        <div className="links">
+            <a href="/">Home</a>
+            <a href="/create">New Blog</a>
+        </div>
+    </nav>
+
+    )
+ 
+}
