68cff4756ab4e28ddd159ae735d896248d8fc14d
mubarak7785 <mubarakjinabade07@gmail.com>
2022-09-16 11:52:16 +0530

    Link

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/create" element={<Create/>}/>
-            
           </Routes>
         </div>
       </div>
